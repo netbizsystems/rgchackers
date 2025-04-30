cf268be601e02afa35fa3027dfa423924a924df6
David A <danderson@netbizsystems.com>
2025-04-30 08:24:51 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   const value = 'RGC Hackers';
   return <>
   <div>Hello {value}</div>
-  <img src="http://rockportgolf.com/wp-content/uploads/2019/05/rgc_logo.png" width="1204" height="1204" alt="Rockport Golf Club" id="logo" data-height-percentage="70" data-actual-width="0" data-actual-height="0"></img>
+  <img src="http://rockportgolf.com/wp-content/uploads/2019/05/rgc_logo.png" 
+    width="100" 
+    height="100" 
+    alt="Rockport Golf Club" id="logo">      
+  </img>
   </>;
 }
 
