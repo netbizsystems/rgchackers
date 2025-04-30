98a668a5d151ac3319068187bc7001cefd97b425
David A <danderson@netbizsystems.com>
2025-04-30 07:55:40 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'RGC Hackers';
-  return <>
-    <Image src="https://reactjs.org/logo-og.png" alt="React Logo" />
-    <div>Hello {value}</div>
-  </>;
+  return <div>Hello {value}</div>;
 }
 
 export default App;
