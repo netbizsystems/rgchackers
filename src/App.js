412139ccf21771fda2871cd98249453a4159d5ce
David A <danderson@netbizsystems.com>
2025-04-30 08:12:19 -0400

    x

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   const value = 'RGC Hackers';
-  return <><div>Hello {value}</div></>;
+  return <>
+  <div>Hello {value}</div>
+  <img src="http://rockportgolf.com/wp-content/uploads/2019/05/rgc_logo.png" width="1204" height="1204" alt="Rockport Golf Club" id="logo" data-height-percentage="70" data-actual-width="0" data-actual-height="0"></img>
+  </>;
 }
 
 export default App;
