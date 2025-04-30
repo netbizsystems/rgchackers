707e4d10ae1e592be33b9040c2332ea2831fee97
David A <danderson@netbizsystems.com>
2025-04-30 10:38:09 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ function App() {
   const value = 'RGC Hackers';
   return <>
   <div>Hello {value}</div>
-  <Button variant="contained" color="primary">Hello World</Button>
-  <Button variant="outlined" color="secondary">Hello World</Button>
-  <Button variant="contained" color="error">Hello World</Button>
-  <Button variant="outlined" color="warning">Hello World</Button>
-  <Button variant="contained" color="info">Hello World</Button>
-  <Button variant="outlined" color="success">Hello World</Button>
-  <Button variant="contained" color="default">Hello World</Button>
+
   <img src="http://rockportgolf.com/wp-content/uploads/2019/05/rgc_logo.png" 
     width="100" 
     height="100" 
