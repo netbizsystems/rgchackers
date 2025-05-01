22fbb866687dc9b1026e98eb1c1dbc7b3c98fa69
David A <danderson@netbizsystems.com>
2025-05-01 07:38:59 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-//import Button from '@mui/material/Button';
 
 function App() {
   const value = 'RGC Hackers';
-  return <>
-  <div>Hello {value}</div>
-
-  <img src="http://rockportgolf.com/wp-content/uploads/2019/05/rgc_logo.png" 
-    width="100" 
-    height="100" 
-    alt="Rockport Golf Club" id="logo">      
-  </img>
-  </>;
+  return <div>Hello {value}</div>;
 }
 
 export default App;
